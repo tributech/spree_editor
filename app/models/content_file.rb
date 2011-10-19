281a4fb1ae1e3c9cadf3ff67d2bf836104522ddd
Colin Goudie <colin.goudie@tributech.com.au>
2011-10-20 09:07:07 +1100

    More removing content file stuff

--- app/models/content_file.rb
+++ app/models/content_file.rb
@@ -1,5 +1,5 @@
 class ContentFile < Asset
-  has_attached_file :attachment,
-    :url => "/assets/content_files/:id/:basename.:extension",
-    :path => ":rails_root/public/assets/content_files/:id/:basename.:extension"
+  #has_attached_file :attachment,
+  #  :url => "/assets/content_files/:id/:basename.:extension",
+  #  :path => ":rails_root/public/assets/content_files/:id/:basename.:extension"
 end
